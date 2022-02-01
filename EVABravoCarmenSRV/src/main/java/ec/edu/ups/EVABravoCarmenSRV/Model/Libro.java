c9388522d82c8e5d184a982dbd94fef300b6a475
CarmenBravo20 <cbravov1@est.ups.edu.ec>
2022-02-01 11:21:14 -0500

    Inicializacion de evaluacion y creacion de paquetes

---
+++ EVABravoCarmenSRV/src/main/java/ec/edu/ups/EVABravoCarmenSRV/Model/Libro.java
@@ -0,0 +1,34 @@
+package ec.edu.ups.EVABravoCarmenSRV.Model;
+
+public class Libro {
+	private int codigo;
+	private String titulo;
+	private String edicion;
+	private String autor;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getEdicion() {
+		return edicion;
+	}
+	public void setEdicion(String edicion) {
+		this.edicion = edicion;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+
+}
